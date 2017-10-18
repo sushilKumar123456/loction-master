ee2b16889a1c2ad911ad1afca1941c53db6ab0de
Sushil <sushil_kumar180@yahoo.com>
2017-10-18 13:08:06 +0530

    cnmcncmn

--- app/src/main/java/com/example/sushil/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/sushil/myapplication/MapsActivity.java
@@ -63,6 +63,7 @@ int x=0;
                 int y=1;
                 int z=2;
                 int bb=00;
+                int vv=0;
 
                 // Animating to the touched position
                 googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
